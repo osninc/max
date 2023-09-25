f4f0162dc090c269b151b951e6ec66cb13358632
Max <mdawa@zoove.com>
2023-09-25 14:01:53 -0400

    test output

--- max/zillow-count/src/orderData.js
+++ max/zillow-count/src/orderData.js
@@ -9,13 +9,36 @@ export const testOrderData = (statusMatrix, timeMatrix, lotSize) => {
     // x axis is lot size
     // const lots = testCounts.filter(x => )
 
-    const result = testCounts.reduce((x, y) => {
+    const result2 = testCounts.reduce((x, y) => {
 
         (x[y.minLotSize] = x[y.minLotSize] || []).push(y);
 
         return x;
 
     }, {});
+
+
+    const result = testCounts.reduce((x, y) => {
+        // let keyName = `${y.minLotSize}-${y.maxLotSize}`;
+
+        // if (y.maxLotSize === "") keyName =  `${y.minLotSize}+`;
+        // if (y.minLotSize === "") keyName = `0-${y.maxLotSize}`;
+
+        // Keyname would be column titles
+        const keyName = `${y.time} - ${y.status}`;
+
+
+        (x[keyName] = x[keyName] || []).push({
+            size: (y.maxLotSize === "") ? `${y.minLotSize}+` : (y.minLotSize === "") ? `0-${y.maxLotSize}` : `${y.minLotSize}-${y.maxLotSize}`,
+            count: y.count,
+            url: ""
+        });
+
+        return x;
+
+    }, {});
+
+
 
 
     // Reduce the reduce
@@ -38,9 +61,12 @@ export const testOrderData = (statusMatrix, timeMatrix, lotSize) => {
         return { [newKey]: timeDim };
     })
 
-    return returnValue;
+    // Fix data that apify would understand
+    //console.log
+
+    return result;
 }
 
-export const orderData = () => {
-    return "hi"
+export const orderData = (statusMatrix, timeMatrix, lotSize) => {
+
 }

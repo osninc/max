7a716c247ca8eb364938d93b75632cb85e7b055c
Max <mdawa@zoove.com>
2023-10-12 17:40:45 -0400

    Changed the timeout to be 10 minutes since apify can take pretty long

--- max/ui/src/axios.js
+++ max/ui/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    timeout: 300000, // 5 minutes
+    timeout: 600000, // 10 minutes
     //baseURL: `${process.env.REACT_APP_API_URL}`,
     responseType: 'json' 
 });

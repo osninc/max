0ac07bd17fec37208ff589e858c028b66fcb1204
Max <mdawa@zoove.com>
2023-12-20 23:11:55 -0500

    testing

--- max/ui/src/components/Copyright.js
+++ max/ui/src/components/Copyright.js
@@ -13,7 +13,7 @@ export const Copyright = (props) => {
             <br />
             build: {process.env.REACT_APP_BUILD_ID}
             <br />
-            built at: {convertDateToLocal(process.env.REACT_APP_BUILD_TIME)}
+            build at: {convertDateToLocal(process.env.REACT_APP_BUILD_TIME)}
         </Typography>
     );
 };
